4b62075366623e14deda71f0f8b1a2b34cd73783
wesdeveloper <wesleyalopes2010@gmail.com>
2021-04-25 23:06:46 -0300

    chore: add typescript setup

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,8 @@ const root = resolve(__dirname);
 module.exports = {
   rootDir: root,
   displayName: 'Unit tests',
-  testMatch: ['<rootDir>/src/**/*.spec.js'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
   testEnvironment: 'node',
   clearMocks: true,
-  verbose: true
+  preset: 'ts-jest',
 };
