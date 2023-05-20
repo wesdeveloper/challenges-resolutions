161f4c36028a93ef20b5a2bb5d8ca5cb60aee4be
Wesley Amaral Lopes <wesleyalopes2010@gmail.com>
2023-05-20 11:32:14 -0300

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # challenges-resolutions
 
 ![codewars](https://www.codewars.com/users/wesdeveloper/badges/large)
+
+
+This is a repository to keep a collection of practice problems from Codewars.
+Codewars is a great resource to practice various coding problems and also is very useful for learning new methods and ways of thinking by studying the clever and best solutions from other users after you submit your solution.
+
+Feel free to check and learn from it.
--- src/codewars/6kyu/roman-numerals-decoder/roman-numerals-decoder.spec.ts
+++ src/codewars/6kyu/roman-numerals-decoder/roman-numerals-decoder.spec.ts
@@ -3,10 +3,10 @@ import { assert } from "chai";
 
 describe("solution", function () {
   it("tests", () => {
-    // assert.equal(solution("XXI"), 21);
-    // assert.equal(solution("I"), 1);
+    assert.equal(solution("XXI"), 21);
+    assert.equal(solution("I"), 1);
     assert.equal(solution("IV"), 4);
-    // assert.equal(solution("MMVIII"), 2008);
-    // assert.equal(solution("MDCLXVI"), 1666);
+    assert.equal(solution("MMVIII"), 2008);
+    assert.equal(solution("MDCLXVI"), 1666);
   });
 });
